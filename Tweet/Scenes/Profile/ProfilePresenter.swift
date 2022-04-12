a9669018fa6cb1f830c9e74937ff896704368fc4
Minji Kim <minji_0801@naver.com>
2022-04-12 14:45:08 +0900

    프로필 화면 구현 재커밋

--- Tweet/Scenes/Profile/ProfilePresenter.swift
+++ Tweet/Scenes/Profile/ProfilePresenter.swift
@@ -6,3 +6,44 @@
 //
 
 import Foundation
+
+protocol ProfileProtocol: AnyObject {
+    func setupViews()
+    func setViews(with name: String, account: String)
+    func endEditing()
+}
+
+final class ProfilePresenter {
+    private weak var viewController: ProfileProtocol?
+    private var user: User {
+        get { User.shared }
+        set { User.shared = newValue }
+    }
+    
+    init(viewController: ProfileProtocol) {
+        self.viewController = viewController
+    }
+    
+    func viewDidLoad() {
+        viewController?.setupViews()
+        viewController?.setViews(with: user.name, account: user.account)
+    }
+    
+    func didTapSaveButton(name: String?, account: String?) {
+        if name == nil || name == "" || account == nil || account == "" {
+            return
+        }
+        
+        viewController?.endEditing()
+        
+        if let name = name {
+            user.name = name
+        }
+        
+        if let account = account {
+            user.account = account
+        }
+        
+        viewController?.setViews(with: user.name, account: user.account)
+    }
+}
--- Tweet/Scenes/Profile/ProfileViewController.swift
+++ Tweet/Scenes/Profile/ProfileViewController.swift
@@ -7,23 +7,90 @@
 
 import UIKit
 
-class ProfileViewController: UIViewController {
+final class ProfileViewController: UIViewController {
+    private lazy var presenter = ProfilePresenter(viewController: self)
+    
+    private lazy var nameTextField: UITextField = {
+        let textField = UITextField()
+        textField.font = .systemFont(ofSize: 20.0, weight: .medium)
+        textField.delegate = self
+
+        return textField
+    }()
+    
+    private lazy var accountTextField: UITextField = {
+        let textField = UITextField()
+        textField.font = .systemFont(ofSize: 16.0, weight: .bold)
+        textField.delegate = self
+
+        return textField
+    }()
+    
+    private lazy var saveButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("저장하기", for: .normal)
+        button.titleLabel?.font = .systemFont(ofSize: 15.0, weight: .bold)
+        button.contentEdgeInsets = UIEdgeInsets(top: 8.0, left: 8.0, bottom: 8.0, right: 8.0)
+        button.layer.cornerRadius = 15.0
+        button.backgroundColor = .systemBlue
+        button.setTitleColor(UIColor.white, for: .normal)
+        button.addTarget(self, action: #selector(didTapSaveButton), for: .touchUpInside)
+
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        presenter.viewDidLoad()
+    }
+}
+
+extension ProfileViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return  true
+    }
+}
+
+extension ProfileViewController: ProfileProtocol {
+    func setupViews() {
+        navigationItem.title = "Profile"
+        
+        [nameTextField, accountTextField, saveButton].forEach {
+            view.addSubview($0)
+        }
+        
+        nameTextField.snp.makeConstraints {
+            $0.leading.trailing.equalToSuperview().inset(16.0)
+            $0.top.equalTo(view.safeAreaLayoutGuide).inset(16.0)
+        }
+        
+        accountTextField.snp.makeConstraints {
+            $0.leading.equalTo(nameTextField.snp.leading)
+            $0.trailing.equalTo(nameTextField.snp.trailing)
+            $0.top.equalTo(nameTextField.snp.bottom).offset(16.0)
+        }
+        
+        saveButton.snp.makeConstraints {
+            $0.leading.equalTo(nameTextField.snp.leading)
+            $0.trailing.equalTo(nameTextField.snp.trailing)
+            $0.top.equalTo(accountTextField.snp.bottom).offset(32.0)
+        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func setViews(with name: String, account: String) {
+        nameTextField.text = name
+        accountTextField.text = account
     }
-    */
+    
+    func endEditing() {
+        view.endEditing(true)
+    }
+}
 
+extension ProfileViewController {
+    @objc func didTapSaveButton() {
+        presenter.didTapSaveButton(name: nameTextField.text, account: accountTextField.text)
+    }
 }
